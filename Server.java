514f6731f6817c6eddd9f7ed67116c96cca4a68a
dani077 <rdani012@gmail.com>
2014-03-07 11:33:26 +0700

    Create Server.java

---
+++ Server.java
@@ -0,0 +1,36 @@
+
+
+	   public static void main(String[] arg) {
+
+	      Employee employee = null;
+
+	      try {
+
+	         ServerSocket socketConnection = new ServerSocket(11111);
+
+	         System.out.println("Server Waiting");
+
+	         Socket pipe = socketConnection.accept();
+
+	         ObjectInputStream serverInputStream = new    
+	            ObjectInputStream(pipe.getInputStream());
+
+	         ObjectOutputStream serverOutputStream = new 
+	            ObjectOutputStream(pipe.getOutputStream());
+
+	         employee = (Employee )serverInputStream.readObject();
+
+	         employee .setEmployeeNumber(256);
+	         employee .setEmployeeName("John");
+
+	         serverOutputStream.writeObject(employee);
+
+	         serverInputStream.close();
+	         serverOutputStream.close();
+
+
+	      }  catch(Exception e) {System.out.println(e); 
+	      }
+	   }
+
+	}
